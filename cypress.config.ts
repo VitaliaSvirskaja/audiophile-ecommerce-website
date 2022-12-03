be17a0929abec790e74d31d8f3839a332b9a6ed1
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-12-03 14:16:42 +0100

    installed cypress

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+// TODO: config base url
+export default defineConfig({
+  e2e: {
+    setupNodeEvents() {
+      // implement node event listeners here
+    },
+  },
+});
