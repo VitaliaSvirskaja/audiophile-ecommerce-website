63bf05709a51793fff93bc15b38dfb3e05859817
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-12-06 10:53:35 +0100

    implemented endpoint for fetching specific products

---
+++ pages/api/products/[productID].ts
@@ -0,0 +1,23 @@
+import path from "path";
+import { promises as fs } from "fs";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Product } from "../../../model/Product";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { productID } = req.query;
+  const jsonDirectory = path.join(process.cwd(), "data");
+  const fileContents = await fs.readFile(jsonDirectory + "/data.json", "utf8");
+  const products: Array<Product> = JSON.parse(fileContents);
+  const foundProduct: Product | undefined = products.find(
+    (product): boolean => product.id.toString() === productID
+  );
+
+  if (foundProduct === undefined) {
+    res.status(404);
+  } else {
+    res.status(200).json(foundProduct);
+  }
+}
