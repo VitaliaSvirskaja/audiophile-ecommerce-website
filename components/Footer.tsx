4f8c4e369c4401ef82e703e3e65cba23ef1f7fb8
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-12-07 21:34:43 +0100

    added responsive design to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,26 +8,39 @@ import { NavLink } from "./NavLink";
 
 export const Footer = () => {
   return (
-    <div className="flex flex-col bg-almost-black text-white px-40 pt-20 pb-12 gap-14">
-      <div className=" flex-col flex items-center gap-10">
-        <div className="flex justify-between w-full max-w-screen-xl">
-          <Image
-            src={logo}
-            alt=""
-            width={143}
-            height={25}
-            className="cursor-pointer"
-          />
-          <div className="flex gap-9">
-            <button className="sub-title tracking-[2px]">Home</button>
-            <button className="sub-title tracking-[2px]">Headphones</button>
-            <button className="sub-title tracking-[2px]">Speakers</button>
-            <button className="sub-title tracking-[2px]">Earphones</button>
+    <div className="absolute bottom-0 flex w-full flex-col items-center bg-almost-black px-6 pb-9 text-white sm:px-10 sm:pb-12">
+      <div className="flex w-full max-w-screen-xl flex-col">
+        <div className="w-24 border-t-4 border-sepia max-sm:m-auto" />
+        <div className="flex items-center justify-between gap-12 pt-14 pb-12 max-lg:flex-col sm:items-start sm:gap-8 sm:pb-8 lg:pt-20">
+          <Link href="/" className="focus-ring p-2" title="Navigate to home">
+            <Image src={logo} alt="" width={143} height={25} />
+          </Link>
+          <div className="flex items-center gap-4 max-sm:flex-col sm:gap-8">
+            <NavLink
+              text="Home"
+              href="/"
+              className="hover:text-sepia active:text-fuchsia"
+            />
+            <NavLink
+              text="Headphones"
+              href="/category/headphones"
+              className="hover:text-sepia active:text-fuchsia"
+            />
+            <NavLink
+              text="Speakers"
+              href="/category/speakers"
+              className="hover:text-sepia active:text-fuchsia"
+            />
+            <NavLink
+              text="Earphones"
+              href="/category/earphones"
+              className="hover:text-sepia active:text-fuchsia"
+            />
           </div>
         </div>
 
-        <div className="flex justify-between w-full max-w-screen-xl">
-          <p className="w-[540px] body-font">
+        <div className="flex flex-col gap-12 max-lg:flex-col sm:gap-20">
+          <p className="body-font text-center opacity-50 sm:text-start lg:w-[540px]">
             Audiophile is an all in one stop to fulfill your audio needs.
             We&apos;re a small team of music lovers and sound specialists who
             are devoted to helping you get the most out of personal audio. Come
