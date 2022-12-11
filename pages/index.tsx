ad93e80d933c6bdfbda5364280c3965e45b9bc5e
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-12-11 21:51:35 +0100

    fixed screen layout of homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,18 @@ import Head from "next/head";
 import { Header } from "../components/Header";
 import { Herosection } from "../components/Herosection";
 import { Footer } from "../components/Footer";
+import { Categories } from "../components/Categories";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex h-screen flex-col overflow-y-auto">
       <Head>
         <title>Audiophile eCommerce</title>
         <meta name="description" content="Audio gear by audiophile" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
+      <main className="flex-1">
         <Herosection />
         <Categories />
       </main>
