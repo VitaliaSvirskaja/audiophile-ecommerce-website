b741e224ee14eefef564bc0c704bf16ddd7a93c4
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-12-13 17:23:15 +0100

    implemented testing of category section

--- cypress/e2e/categories.cy.ts
+++ cypress/e2e/categories.cy.ts
@@ -36,4 +36,40 @@ describe("categories", () => {
     cy.wait(100);
     cy.url().should("include", "/category/earphones");
   });
+  it("should include the category headphones", () => {
+    cy.get("[title='menu']").click().siblings().first().contains("headphones");
+  });
+  it("should include the category speakers", () => {
+    cy.get("[title='menu']").click().siblings().first().contains("speakers");
+  });
+  it("should include the category earphones", () => {
+    cy.get("[title='menu']").click().siblings().first().contains("earphones");
+  });
+  it("should include the headphones category image", () => {
+    cy.get("[title='menu']")
+      .click()
+      .siblings()
+      .first()
+      .find("img")
+      .first()
+      .should("have.attr", "alt", "headphones");
+  });
+  it("should include the earphones category image", () => {
+    cy.get("[title='menu']")
+      .click()
+      .siblings()
+      .first()
+      .find("img")
+      .eq(7)
+      .should("have.attr", "alt", "earphones");
+  });
+  it("should include the speakers category image", () => {
+    cy.get("[title='menu']")
+      .click()
+      .siblings()
+      .first()
+      .find("img")
+      .eq(3)
+      .should("have.attr", "alt", "speakers");
+  });
 });
