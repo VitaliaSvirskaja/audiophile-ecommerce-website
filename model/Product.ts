de1ae02df3fb3454ae350e0d8112b4d3527a5fe5
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-12-19 21:07:14 +0100

    added included items to the product interface

--- model/Product.ts
+++ model/Product.ts
@@ -1,6 +1,12 @@
 // TODO: add missing properties
 
+export interface IncludedItems {
+  quantity: number;
+  item: string;
+}
+
 export interface Product {
   id: number;
   name: string;
+  includes: Array<IncludedItems>;
 }
