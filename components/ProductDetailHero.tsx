a5f166d0c7dec01761e2ef9a5c4234be4171c426
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-12-24 10:34:47 +0100

    added add to cart component into product detail hero (without logic)

--- components/ProductDetailHero.tsx
+++ components/ProductDetailHero.tsx
@@ -1,27 +1,21 @@
 import Image from "next/image";
 import { Product } from "../model/Product";
-import { useEffect, useState } from "react";
+import { AddToCart } from "./AddToCart";
+import { currencyFormatter } from "../utils/currencyFormatter";
 
 interface Props {
   product: Product | undefined;
 }
 
 export const ProductDetailHero = ({ product }: Props) => {
-  const formatter =
-    new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-    }) || 0;
-
-  const [fixedPrice, setFixedPrice] = useState("");
-
-  useEffect(() => {
-    if (product?.price === undefined) {
+  function handleAddToCart(counter: number) {
+    if (product === undefined) {
       return;
-    } else {
-      setFixedPrice(product.price.toFixed(0));
     }
-  }, [product]);
+    const price = counter * product?.price;
+    console.log(price);
+  }
+
   return (
     <div className="m-auto my-8 flex w-full max-w-screen-xl gap-8 max-sm:flex-col sm:gap-16 lg:gap-32">
       <div className="flex w-full items-center rounded-lg bg-middle-grey">
@@ -55,8 +49,11 @@ export const ProductDetailHero = ({ product }: Props) => {
         <div className="flex flex-col gap-6 sm:gap-8">
           <div className="h4 lg:h2 tracking-wider">{product?.name}</div>
           <div className="body-font opacity-50">{product?.description}</div>
-          <div className="h6">{formatter.format(parseInt(fixedPrice))}</div>
+          <div className="h6">
+            {currencyFormatter.format(product?.price || 0)}
+          </div>
         </div>
+        <AddToCart onAddToCart={handleAddToCart} />
       </div>
     </div>
   );
