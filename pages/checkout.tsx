ad41d92db8d7776d3ce6b42c7fd6c8ff14840d35
vitaliasvirskaja <vitalia.svirskaja.vs@gmail.com>
2022-12-29 14:30:30 +0100

    fixed styling

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,19 +1,23 @@
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
-import Link from "next/link";
 import { CheckoutSummary } from "../components/CheckoutSummary";
 import { CheckoutForm } from "../components/CheckoutForm";
+import { useRouter } from "next/router";
 
 export const Checkout = () => {
+  const router = useRouter();
   return (
     <div className="bg-middle-grey">
       <Header />
       <main className="my-8 px-6 md:px-10">
         <div className="m-auto flex max-w-screen-xl flex-col gap-10 bg-middle-grey">
           <div>
-            <Link href={`/`} className="body-font opacity-50">
-              GO BACK
-            </Link>
+            <button
+              onClick={() => router.back()}
+              className="body-font opacity-50 hover:text-sepia"
+            >
+              Go Back
+            </button>
           </div>
           <div className="flex justify-between gap-8 max-lg:flex-col">
             <CheckoutForm />
